19ce5e811b6599fac915aa77ef0c11b1a7454dde
Rakshitha <112706233+Rakshithaashok@users.noreply.github.com>
2023-10-08 21:09:04 +0530

    Add files via upload

---
+++ ContentManagementTool/footer.php
@@ -0,0 +1,35 @@
+<footer id="footer">
+  <div id="slideContainer">
+    <div id="slide1">
+      <div id="desc">
+        <div id="title">PMS (Project Management System)</div>
+        <div id="details">
+          Project Management System in PHP is a platform where users can communicate and assign tasks to others
+        </div>
+      </div>
+      <div id="socialBtnContainer">
+        <a href="https://www.facebook.com/"><i
+            class="fa-brands fa-square-facebook"></i></a>
+        <a href="https://instagram.com/rakshitha_0724?igshid=OGQ5ZDc2ODk2ZA=="><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/rakshitha-ashok-0a04bb224/"><i class="fa-brands fa-linkedin"></i></a>
+      </div>
+      <div id="feedbackBtnContainer">
+        <button type="submit" id="feedbackBtn">Share your feedback!</button>
+      </div>
+    </div>
+
+    <div id="slide2">
+      <div id="feedbackFormContainer">
+        <button id="feedbackBackBtn">
+          <i class="fa-solid fa-backward"></i>
+        </button>
+        <div id="feedback">
+          <div id="title">Write your Feedback!</div>
+          <textarea id="feedbackContent" placeholder="Write here..."></textarea>
+          <button id="shareFeedback" type="submit">Share</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="developed">developed by @Rakshitha</div>
+</footer>
